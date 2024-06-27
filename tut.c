6856f0b96e9dfc4467e6e37410b5ea5ab5838e66
Omega Paulo <paulodafiotiomega@outlook.com>
2024-06-27 11:52:55 +0300

    Task 3: Temperature Converter

--- tut.c
+++ tut.c
@@ -31,28 +31,43 @@ int main() {
 //* Task 2: Arithmetic Operations
 // Create a program that prompts the user to enter two integers.Perform addition, subtraction, multiplication, division, and modulus operations on these numbers and display the results.
 
-  int num1;
-  int num2;
-  char modulos[] = "%";
-  printf("Enter first number:");
-  scanf("%d", &num1);
-  printf("Enter second number:");
-  scanf("%d", &num2);
+  // int num1;
+  // int num2;
+  // char modulos[] = "%";
+  // printf("Enter first number:");
+  // scanf("%d", &num1);
+  // printf("Enter second number:");
+  // scanf("%d", &num2);
 
-  int add = num1 + num2;
-  int sub = num1 - num2;
-  int mult = num1 * num2;
-  float divi = (float)num1 / (float)num2;
-  int mod = num1 % num2;
+  // int add = num1 + num2;
+  // int sub = num1 - num2;
+  // int mult = num1 * num2;
+  // float divi = (float)num1 / (float)num2;
+  // int mod = num1 % num2;
 
-  printf("Your first number is %d and your second number is %d\n", num1, num2);
-  printf("Results:\n");
+  // printf("Your first number is %d and your second number is %d\n", num1, num2);
+  // printf("Results:\n");
 
-  printf("%d + %d = %d\n", num1, num2, add);
-  printf("%d - %d = %d\n", num1, num2, sub);
-  printf("%d * %d = %d\n", num1, num2, mult);
-  printf("%.0f / %.0f = %.2f\n", (float)num1, (float)num2, divi);
-  printf("%d %s %d = %d", num1, modulos, num2, mod);
+  // printf("%d + %d = %d\n", num1, num2, add);
+  // printf("%d - %d = %d\n", num1, num2, sub);
+  // printf("%d * %d = %d\n", num1, num2, mult);
+  // printf("%.0f / %.0f = %.2f\n", (float)num1, (float)num2, divi);
+  // printf("%d %s %d = %d", num1, modulos, num2, mod);
+
+
+  //* Task 3: Temperature Converter
+  // Write a program that converts a temperature from Fahrenheit to Celsius and Kelvin.Ask the user to input the temperature in Fahrenheit.
+
+  float fahr;
+
+  printf("Enter a temp in fahrenheit:");
+  scanf("%f", &fahr);
+
+  float cels = (fahr - 32) * 5 / 9;
+  float kelv = cels + 273.15;
+
+  printf("Temperature in Celsius: %.2f\n", cels);
+  printf("Temperature in Kelvin: %.2f", kelv);
 
 
   return 0;
